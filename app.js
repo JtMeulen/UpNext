de83026af107f2d0c271e0721d8d37d27cd9a0c2
Joey ter Meulen <joeytermeulen@gmail.com>
2017-12-21 20:49:55 +0000

    Setup basic files and folders

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+    app     = express();
+    
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.redirect("/index");
+});
+
+app.get("/index", function(req, res){
+    res.render("index");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER STARTED ON PORT " + process.env.PORT);
+});
